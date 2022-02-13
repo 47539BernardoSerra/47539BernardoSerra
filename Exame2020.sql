4bf39389e94ccdecbace7cbbfb930bf7b71d921d
47539BernardoSerra <80893506+47539BernardoSerra@users.noreply.github.com>
2022-02-13 23:52:07 +0000

    Update Exame2020.sql

--- Exame2020.sql
+++ Exame2020.sql
@@ -1,4 +1,7 @@
 --d
+Apresente uma instrução T-SQL que permita obter a lista dos clientes (nome), que moram na
+cidade de Lisboa, que usaram o serviço “urgente” e , que nunca tenham usado o “não urgente”, ordenado
+
 	select p.nome
 		from pessoa p
 		where p.cidade = 'Lisboa'
@@ -15,6 +18,9 @@
 		order by p.nome
 		
 --f
+Apresente uma instrução T-SQL que indique o número de encomendas por cliente cuja a data de
+entrega é superior a 25 dias após a data de compra. Utilize obrigatóriamente a claúsula GROUP BY
+
 	select a.nocliente, count(a.nocliente)
 		from (
 			select e.nocliente, e.dtentrega - e.dtcompra as dt
