ff174234c6549cccb529900e34ddc36661e8ddf4
47539BernardoSerra <80893506+47539BernardoSerra@users.noreply.github.com>
2022-02-13 23:40:23 +0000

    Update Exame2015.sql

--- Exame2015.sql
+++ Exame2015.sql
@@ -1,4 +1,7 @@
---c
+--c 
+Escreva uma expressão de álgebra relacional que permita saber os detalhes dos jogos da jornada nº1.
+Apresente o nome da equipa da casa, os seus golos, o nome da equipa visitante e os seus golos. 
+
 	select e.nome, j.golosequipacasa, e2.nome, j.golosequipafora 
 		from jogo j 
 			inner join equipa e on e.refe = j.refequipacasa 
@@ -6,6 +9,9 @@
 			where j.njornada  = 12;
 
 --e
+Escreva uma expressão de álgebra relacional que permita obter a lista de jogadores (nome e bi) com o
+total de golos para cada jogador.
+
 	select j.nome, count(g.bi)
 		from jogador j 
 			inner join golo g on g.bi = j.bi 
@@ -13,6 +19,9 @@
 			order by count desc;
 
 --f
+Escreva uma instrução T-SQL que permita obter uma lista da(s) equipa(s) que ganhou mais jogos em
+todas as jornadas registadas. Tenha em atenção os jogos em casa e fora. 
+
 	select	a.refequipacasa, sum(a.count)
 		from(
 			select j.refequipacasa, count(j.refequipacasa)
@@ -28,6 +37,8 @@
 			group by a.refequipacasa
 			
 --g
+Escreva uma instrução T-SQL que permita obter as equipas que num jogo já marcaram mais de 4 golos
+e que num jogo nunca sofreram mais de 3 golos.	
 	
 	select e.refe, e.nome
 		from equipa e 
